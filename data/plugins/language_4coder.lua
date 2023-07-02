084d5d0a7e181654900fd435ce3fa240254585f5
maihd.dev <maihd.dev@gmail.com>
2023-07-02 18:43:11 +0700

    language_4coder.lua

---
+++ data/plugins/language_4coder.lua
@@ -0,0 +1,27 @@
+local syntax = require "core.syntax"
+
+syntax.add {
+    files = { "%.4coder$" },
+    comment = "//",
+    patterns = {
+        { pattern = "//.-\n",               type = "comment"  },
+        { pattern = { "/%*", "%*/" },       type = "comment"  },
+        { pattern = { '"', '"', '\\' },     type = "string"   },
+        { pattern = { "'", "'", '\\' },     type = "string"   },
+        { pattern = "-?0x%x+",              type = "number"   },
+        { pattern = "-?%d+[%d%.eE]*f?",     type = "number"   },
+        { pattern = "-?%.?%d+f?",           type = "number"   },
+        { pattern = "\\\n",                 type = "keyword2" },
+        { pattern = "[%+%-=/%*%^%%<>!~|&]", type = "operator" },
+        { pattern = "[%a_][%w_]*[ ]*=",     type = "function" },
+        { pattern = "[%a_][%w_]*",          type = "symbol"   },
+    },
+    symbols = {
+        -- @note: does we need atomic_t highlights?
+
+        -- C11 literal
+        ["true"]     = "literal",
+        ["false"]    = "literal",
+    },
+}
+
