869aae129d213a3868b49766d62ddaa144cf9131
maihd <maihd.dev@gmail.com>
2023-07-04 08:24:31 +0700

    some change in lite_window_win32.c

--- src/platforms/lite_window_win32.c
+++ src/platforms/lite_window_win32.c
@@ -6,31 +6,12 @@
 #include <Windows.h>
 #endif
 
-#include <SDL2/SDL.h>
-
-#ifdef _WIN32
-//#include <windows.h>
-#elif __linux__
-#include <unistd.h>
-#elif __APPLE__
-#include <mach-o/dyld.h>
-#endif
-
 #include "lite_window.h"
 
-SDL_Window* window;
+HWND window;
 
 static void lite_window_load_icon(void)
 {
-#if !defined(_WIN32) && !defined(__MINGW32__)
-#include "../res/icon.inl"
-    (void)icon_rgba_len; /* unused */
-    SDL_Surface* surf =
-        SDL_CreateRGBSurfaceFrom(icon_rgba, 64, 64, 32, 64 * 4, 0x000000ff,
-                                 0x0000ff00, 0x00ff0000, 0xff000000);
-    SDL_SetWindowIcon(window, surf);
-    SDL_FreeSurface(surf);
-#endif
 }
 
 void lite_console_open(void)
@@ -52,7 +33,6 @@ void lite_console_close(void)
 
 void lite_window_open(void)
 {
-#ifdef _WIN32
     HINSTANCE lib = LoadLibraryA("user32.dll");
     int (*SetProcessDPIAware)() =
     (void*)GetProcAddress(lib, "SetProcessDPIAware");
@@ -60,22 +40,26 @@ void lite_window_open(void)
     {
         SetProcessDPIAware();
     }
-#endif
+    SetThreadExecutionState(ES_CONTINUOUS);
     
-    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
-    SDL_EnableScreenSaver();
-    SDL_EventState(SDL_DROPFILE, SDL_ENABLE);
-    atexit(SDL_Quit);
+    //SDL_EventState(SDL_DROPFILE, SDL_ENABLE);
+    //atexit(SDL_Quit);
     
-#ifdef SDL_HINT_VIDEO_X11_NET_WM_BYPASS_COMPOSITOR /* Available since 2.0.8 */
-    SDL_SetHint(SDL_HINT_VIDEO_X11_NET_WM_BYPASS_COMPOSITOR, "0");
-#endif
-#if SDL_VERSION_ATLEAST(2, 0, 5)
-    SDL_SetHint(SDL_HINT_MOUSE_FOCUS_CLICKTHROUGH, "1");
-#endif
+    // Acquire focus when mouse click
+    //#if SDL_VERSION_ATLEAST(2, 0, 5)
+    //SDL_SetHint(SDL_HINT_MOUSE_FOCUS_CLICKTHROUGH, "1");
+    //#endif
+    
+    // Get current display monitor size
+#if 0
+    HMONITOR hMonitor = ;
+    MONITORINFO mi;
     
     SDL_DisplayMode dm;
     SDL_GetCurrentDisplayMode(0, &dm);
+#else
+    // Use old-style function to get monitor size
+#endif
     
     window = SDL_CreateWindow("", 
                               SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, (int)(dm.w * 0.8), (int)(dm.h * 0.8),
