eb511c05340c03d4a98ed95fce60760c13dd54b1
maihd <maihd.dev@gmail.com>
2023-07-05 09:57:10 +0700

    review status_view, attempt to dispay language in status bar

---
+++ data/plugins/language_license.lua
@@ -0,0 +1,21 @@
+local syntax = require "core.syntax"
+
+syntax.add {
+  files = { "%LICENSE$", "%LICENSE.txt$", "%LICENSE.md$" },
+  patterns = {
+--     { pattern = "\\.",                    type = "normal"   },
+--     { pattern = { "<!%-%-", "%-%->" },    type = "comment"  },
+--     { pattern = { "```", "```" },         type = "string"   },
+--     { pattern = { "``", "``", "\\" },     type = "string"   },
+--     { pattern = { "`", "`", "\\" },       type = "string"   },
+--     { pattern = { "~~", "~~", "\\" },     type = "keyword2" },
+--     { pattern = "%-%-%-+",                type = "comment" },
+--     { pattern = "%*%s+",                  type = "operator" },
+--     { pattern = { "%*", "[%*\n]", "\\" }, type = "operator" },
+--     { pattern = { "%_", "[%_\n]", "\\" }, type = "keyword2" },
+--     { pattern = "#.-\n",                  type = "keyword"  },
+--     { pattern = "!?%[.-%]%(.-%)",         type = "function" },
+    { pattern = "^Copyright",             type = "function" },
+  },
+  symbols = { },
+}
