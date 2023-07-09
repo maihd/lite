ad8cc240198a203de578844aad3271ffe2fe7eff
maihd.dev <maihd.dev@gmail.com>
2023-07-10 00:13:58 +0700

    working on read directory each files instead of all by once

--- src/lite_file.h
+++ src/lite_file.h
@@ -2,8 +2,15 @@
 
 #include "lite_meta.h"
 #include "lite_string.h"
+#include "lite_memory.h"
 
-bool lite_is_binary_file(LiteStringView path);
-uint64_t lite_file_write_time(LiteStringView string);
+typedef struct LiteReadDirState LiteReadDirState;
+
+bool                lite_is_binary_file(LiteStringView path);
+uint64_t            lite_file_write_time(LiteStringView string);
+
+LiteReadDirState*   lite_read_dir(LiteArena* arena, LiteStringView string);
+LiteStringView      lite_read_next_file(LiteReadDirState* read_dir_state);
 
 //! EOF
+
