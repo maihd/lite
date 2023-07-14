81848ecb93f00a038085119d50871544467eaadb
maihd.dev <maihd.dev@gmail.com>
2023-07-14 23:16:37 +0700

    fix language_lua.lua

--- data/plugins/language_lua.lua
+++ data/plugins/language_lua.lua
@@ -24,9 +24,9 @@ syntax.add {
         { pattern = "%.%.%.?",                type = "operator" },
         { pattern = "[<>~=]=",                type = "operator" },
         { pattern = "[%+%-=/%*%^%%#<>]",      type = "operator" },
-        { pattern = "[%a_][%w_]*%s*%f[(\"{]", type = "function" },
         { pattern = "[%a_][%w_]*",            type = "symbol"   },
-        { pattern = "::[%a_][%w_]*::",        type = "function" },
+        { pattern = "[%a_][%w_]*%s*%f[(\"{]", type = "function" },
+        { pattern = "::[%a_][%w_]*::",        type = "function" }, -- @todo(maihd): label highlight
     },
     symbols = {
         ["if"]       = "keyword",
