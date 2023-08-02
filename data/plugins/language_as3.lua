e5aba46c26a2d4e6b60e53dc9d68ea788e4a018f
maihd <maihd.dev@gmail.com>
2023-08-02 11:01:10 +0700

    simple language_as3.lua

---
+++ data/plugins/language_as3.lua
@@ -0,0 +1,76 @@
+local syntax = require "core.syntax"
+
+syntax.add {
+    name = "ActionScript3",
+    files = { "%.as$" },
+    comment = "//",
+    scope_begin = { "{" },
+    scope_end = { "}" },
+    patterns = {
+        { pattern = "//.-\n",               type = "comment"  },
+        { pattern = { "/%*", "%*/" },       type = "comment"  },
+--         { pattern = { "#", "[ \n]" },       type = "keyword2" },
+        { pattern = { '"', '"', '\\' },     type = "string"   },
+        { pattern = { "'", "'", '\\' },     type = "string"   },
+--         { pattern = "<[[%a_][%w_]*.\\]*>",  type = "string"   }, -- @note(maihd): trick to highlight include path
+        { pattern = "-?0x%x+",              type = "number"   },
+        { pattern = "-?%d+[%d%.eE]*f?",     type = "number"   },
+        { pattern = "-?%.?%d+f?",           type = "number"   },
+--         { pattern = "\\\n",                 type = "keyword2" }, -- @note(maihd): end line of macros
+        { pattern = "[%+%-=/%*%^%%<>!~|&]", type = "operator" },
+        { pattern = "[%a_][%w_]*%f[(]",     type = "function" },
+        { pattern = "[%a_][%w_]*",          type = "symbol"   },
+        -- @todo(maihd): add label highlight
+    },
+    symbols = {
+        -- Basic keywords
+        ["if"]          = "keyword",
+        ["else"]        = "keyword",
+        ["do"]          = "keyword",
+        ["while"]       = "keyword",
+        ["for"]         = "keyword",
+        ["break"]       = "keyword",
+        ["continue"]    = "keyword",
+        ["return"]      = "keyword",
+        ["goto"]        = "keyword",
+        ["class"]       = "keyword",
+        ["static"]      = "keyword",
+        ["switch"]      = "keyword",
+        ["case"]        = "keyword",
+        ["default"]     = "keyword",
+        ["const"]       = "keyword",
+        ["public"]      = "keyword",
+        ["private"]     = "keyword",
+        ["package"]     = "keyword",
+        ["function"]    = "keyword",
+        ["get"]         = "keyword",
+        ["set"]         = "keyword",
+
+        ["null"]        = "literal",
+        ["true"]        = "literal",
+        ["false"]       = "literal",
+
+        -- Basic types
+        ["void"]     = "keyword2",
+        ["int"]      = "keyword2",
+        ["uint"]     = "keyword2",
+        ["Array"]    = "keyword2",
+        ["Object"]   = "keyword2",
+        ["String"]   = "keyword2",
+        ["Number"]   = "keyword2",
+        ["Boolean"]  = "keyword2",
+
+        -- Size integer types
+        ["int8_t"]   = "keyword2",
+        ["int16_t"]  = "keyword2",
+        ["int32_t"]  = "keyword2",
+        ["int64_t"]  = "keyword2",
+        ["uint8_t"]  = "keyword2",
+        ["uint16_t"] = "keyword2",
+        ["uint32_t"] = "keyword2",
+        ["uint64_t"] = "keyword2",
+        ["size_t"]   = "keyword2",
+        ["intptr_t"] = "keyword2",
+        ["uintptr_t"] = "keyword2",
+    },
+}
