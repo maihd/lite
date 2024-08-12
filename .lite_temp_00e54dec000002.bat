a153721f2ad20f79cbd48d54f85d13a666124d78
maihd <maihd.dev@gmail.com>
2024-08-12 17:08:52 +0700

    remove unrelated files

--- .lite_temp_00e54dec000002.bat
+++
@@ -1,5 +0,0 @@
-        @echo off
-        call "G:\\projects\\lite\\.lite_temp_00e54dec000001.bat" >"G:\\projects\\lite\\.lite_temp_00e54dec000003" 2>&1
-        echo "" >"G:\\projects\\lite\\.lite_temp_00e54dec000004"
-        exit
-      
